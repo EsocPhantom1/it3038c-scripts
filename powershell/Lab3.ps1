be3a1041f29b4b4439550dc303ab7a4389a0f411
EsocPhantom1 <142767955+EsocPhantom1@users.noreply.github.com>
2023-09-12 21:23:36 -0400

    Add files via upload

--- powershell/Lab3.ps1
+++ powershell/Lab3.ps1
@@ -4,4 +4,4 @@ $HostName = $env:COMPUTERNAME
 $PowerShellVersion = $HOST.Version.Major
 $CurrentDate =  Get-Date -Format "dddd, MMMM dd, yyyy"
 $BODY = "This machine's IP address is $IPAddress. User is $CurrentUser. Hostname is $Hostname. Powershell Version $PowerShellVersion. Today's Date is $CurrentDate."
-Send-MailMessage -To "faissame@mail.uc.edu" -From "faissame2001@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "faissame2001@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
