6370472a0af53d02d5dc4e76cb8e6046ecc9fd62
EsocPhantom1 <142767955+EsocPhantom1@users.noreply.github.com>
2023-09-27 23:54:21 -0400

    Update Lab5.py

--- labs/Lab5.py
+++ labs/Lab5.py
@@ -3,13 +3,11 @@ from datetime import datetime
 def calculate_age_in_seconds(birthday):
     # Calculate the current date and time
     currentDate = datetime.now()
-
     # Calculate the difference between the current date and the birthday
     age = currentDate - birthday
-
     # Convert the age to seconds
     ageInSeconds = age.total_seconds()
-
+    
     return ageInSeconds
 
 # Ask the user to enter their birthday in yyyy-mm-dd format
@@ -18,10 +16,8 @@ birthdayStr = input("Enter your birthday (mm-dd-yyyy): ")
 try:
     # Parse user input into datetime object
     birthday = datetime.strptime(birthdayStr, '%m-%d-%Y')
-
     # Calculate age in seconds
     seconds = calculate_age_in_seconds(birthday)
-
     # Display results
     print(f"You are approximately {seconds:.0f} seconds old.")
 except ValueError:
