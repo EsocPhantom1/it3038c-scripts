3560ead0e06c50fe3a5419908479e6d13ac29116
EsocPhantom1 <142767955+EsocPhantom1@users.noreply.github.com>
2023-10-31 00:15:25 -0400

    Add files via upload

---
+++ labs/lab9/Lab9.py
@@ -0,0 +1,24 @@
+import requests
+
+# url where Node.js is running
+node_api_url = "http://localhost:3001/"
+
+try:
+    # Make a GET request to the Node.js API for all widgets
+    response_all_widgets = requests.get(f"{node_api_url}")
+
+    # Check if the request was successful (status code 200)
+    if response_all_widgets.status_code == 200:
+        # Parse JSON response
+        widgets_all = response_all_widgets.json()
+
+        # Print all widget names and colors 
+        print("All widgets:")
+        for widget in widgets_all:
+            print(f"{widget['name']} is {widget['color']}.")
+
+    else:
+        print(f"Error: {response_all_widgets.status_code} - {response_all_widgets.text}")
+
+except requests.RequestException as e:
+    print(f"Request failed: {e}")
---
+++ labs/lab9/widget.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const data = [
+  { name: 'Widget1', color: 'blue' },
+  { name: 'Widget2', color: 'green' },
+  // Add more widget data as needed
+];
+
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+  } else if (req.url === "/blue") {
+    const blueWidgets = data.filter(widget => widget.color === 'blue');
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(blueWidgets));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Data not found");
+  }
+});
+
+const PORT = 3001; // Use port 3001
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
