88cb0d3faad7b4d8689b9e12804688e84fd2791c
EsocPhantom1 <142767955+EsocPhantom1@users.noreply.github.com>
2023-10-31 00:16:56 -0400

    Update widget.js

--- labs/lab9/widget.js
+++ labs/lab9/widget.js
@@ -3,7 +3,7 @@ const http = require('http');
 const data = [
   { name: 'Widget1', color: 'blue' },
   { name: 'Widget2', color: 'green' },
-  // Add more widget data as needed
+  
 ];
 
 const server = http.createServer(function (req, res) {
@@ -20,7 +20,7 @@ const server = http.createServer(function (req, res) {
   }
 });
 
-const PORT = 3001; // Use port 3001
+const PORT = 3001; 
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
