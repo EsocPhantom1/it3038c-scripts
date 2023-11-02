c2dd86a826477dcc6cfa92b030603b8e8a93874d
EsocPhantom1 <142767955+EsocPhantom1@users.noreply.github.com>
2023-11-02 14:55:57 -0400

    Update widget.js

--- labs/lab9/widget.js
+++ labs/lab9/widget.js
@@ -42,7 +42,7 @@ function serveHtmlPage(res, content) {
   res.end(htmlContent);
 }
 
-const PORT = 3001;
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
